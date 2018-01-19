8251ebea15600a2addfcffb540aa160ee6948193
Alevtina Lobova <ally1125@gmail.com>
2018-01-19 23:57:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ function destructivelyRemoveLastKitten(name){
 function destructivelyRemoveFirstKitten(name){
   kittens.shift()
 }
+function appendKitten(name){
+  kittens.push(name)
+  return newArray = [appendKitten();]
+}
